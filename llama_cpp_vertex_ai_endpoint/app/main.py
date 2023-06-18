23b6ab0c0c2fb8d1c23b7b96496adfcab11dad4b
cmsm12 <dohwankim9@googlemail.com>
2023-06-18 22:46:03 +0200

    fix request/response path and format to comply vertex ai requirements

--- llama_cpp_vertex_ai_endpoint/app/main.py
+++ llama_cpp_vertex_ai_endpoint/app/main.py
@@ -1,22 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from llama_cpp import Llama
-from typing import List, Optional
 import os
 
+AIP_HEALTH_ROUTE = os.environ.get('AIP_HEALTH_ROUTE', '/health')
+AIP_PREDICT_ROUTE = os.environ.get('AIP_PREDICT_ROUTE', '/predict')
 MODEL_PATH = "./models/Wizard-Vicuna-7B-Uncensored.ggmlv3.q4_0.bin"
+
 app = FastAPI()
 
 
-@app.get("/", status_code=200)
+# check requirements for custom container at: "https://cloud.google.com/vertex-ai/docs/predictions/custom-container-requirements"
+@app.get(AIP_HEALTH_ROUTE, status_code=200)
 async def handle_health_check():
     """ health check. This should be on root for default in vertex AI endpoint"""
     return {"health": "ok"}
 
 
-@app.post("/predict")
-async def handle_predict(query: str, echo: bool = False, stop: Optional[List[str]] = None):
+@app.post(AIP_PREDICT_ROUTE)
+async def handle_predict(request: Request):
     """ health check. This should be on root for default in vertex AI endpoint"""
-    llm = Llama(model_path=MODEL_PATH)
-    output = llm(query, max_tokens=200, echo=echo, stop=stop)
-    print(output)
-    return {"output": output["choices"][0]["text"]}
+    body = await request.json()
+    outputs = []
+    for question in body['instances']:
+        llm = Llama(model_path=MODEL_PATH)
+        output = llm(question['query'], max_tokens=200)
+        outputs.append({"answer": output["choices"][0]["text"]})
+    return {"predictions": outputs}
--- llama_cpp_vertex_ai_endpoint/deploy_script.sh
+++ llama_cpp_vertex_ai_endpoint/deploy_script.sh
@@ -9,13 +9,18 @@ DEPLOYMENT_NAME="llama_cpp_model_deployment"
 # build with cloudbuild
 gcloud builds submit --config=cloudbuild.yaml \
   --substitutions=_LOCATION=$LOCATION,_REPOSITORY=$REPOSITORY_NAME,_IMAGE=$IMAGE_NAME .
-
+  
 # upload model
+# make sure the port is same as the port set for docker container
 gcloud ai models upload \
   --region=europe-west3 \
   --display-name=$MODEL_DISPLAY_NAME \
-  --container-image-uri="$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$IMAGE_NAME"
-
+  --container-image-uri="$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$IMAGE_NAME" \
+  --container-ports=8080 \
+  --container-predict-route="/predict" \
+  --container-health-route="/health"
+    
+  
 # create endpoint
 gcloud ai endpoints create \
   --project=$PROJECT_ID \
@@ -33,4 +38,5 @@ gcloud ai endpoints deploy-model $ENDPOINT_ID \
   --model=$MODEL_ID \
   --traffic-split=0=100 \
   --machine-type="n1-standard-2" \
-  --display-name=$DEPLOYMENT_NAME
+  --display-name=$DEPLOYMENT_NAME \
+  --enable-access-logging
